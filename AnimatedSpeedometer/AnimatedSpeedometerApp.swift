2a033810488dfa87f821419a858836c9fa05e465
Ahmad Azam <ahmadazam950@gmail.com>
2025-08-03 22:53:33 +0500

    Initial Commit

---
+++ AnimatedSpeedometer/AnimatedSpeedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedSpeedometerApp.swift
+//  AnimatedSpeedometer
+//
+//  Created by Ahmad Azam on 03/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedSpeedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
